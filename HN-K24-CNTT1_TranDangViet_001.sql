629cf5f162e0b9da739ab994c8fb49b3b03a5245
Đăng Việt <trandangviet00@gmail.com>
2026-01-21 10:09:26 +0700

    Add files via upload

--- HN-K24-CNTT1_TranDangViet_001.sql
+++ HN-K24-CNTT1_TranDangViet_001.sql
@@ -13,7 +13,7 @@ create table Shippers (
 create table Vehicle_Details (
     vehicle_id int primary key,
     shipper_id int,
-    license_plate varchar(20) ,
+    license_plate varchar(20) unique,   -- ✅ thêm UNIQUE
     vehicle_type enum('Tải', 'Xe máy', 'Container'),
     max_payload int check (max_payload > 0),
     foreign key (shipper_id) references Shippers(shipper_id)
@@ -46,36 +46,35 @@ create table Delivery_Log (
     note varchar(255),
     foreign key (order_id) references Delivery_Orders(order_id)
 );
+insert into Shippers(shipper_id, full_name, phone, license_type, rating) values
+(1, 'Nguyen Van An', '0901234567','C', 4.8),
+(2, 'Tran Thi Binh', '0912345678','A2', 5.0),
+(3, 'Le Hoang Nam', '0983456789','FC', 4.2),
+(4, 'Pham Minh Duc', '0354567890','B2', 4.9),
+(5, 'Hoang Quoc Viet','0775678901','C', 4.7);
 
-insert into Shippers(shipper_id, full_name,phone, license_type,rating) values
-(1, 'Nguyen Van An', 0901234567,'C', 4.8),
-(2, 'Tran Thi Binh', 0912345678, 'A2', 5.0),
-(3, 'Le Hoang Nam', 0983456789, 'FC', 4.2),
-(4, 'Pham Minh Duc', 0354567890, 'B2',4.9),
-(5, 'Hoang Quoc Viet',0775678901,'C', 4.7);
-
-insert into Vehicle_Details(vehicle_id, shipper_id, license_plate, vehicle_type, max_payload) values
+insert into Vehicle_Details values
 (101, 1, '29C-123.45','Tải',3500),
 (102, 2, '59A-888.88','Xe máy',500),
 (103, 3, '15R-999.99','Container',32000),
 (104, 4, '30F-111.22','Tải',1500),
-(105, 5, '43C-444.55','Tải', 5000);
+(105, 5, '43C-444.55','Tải',5000);
 
-insert into Shipments(shipment_id, product_name, actual_weight, product_value, ship_status) values
+insert into Shipments values
 (5001,'Smart TV Samsung 55 inch',25.5,15000000,'In Transit'),
 (5002,'Laptop Dell XPS',2.0,35000000,'Delivered'),
 (5003,'Máy nén khí công nghiệp',450.0,120000000,'In Transit'),
 (5004,'Thùng trái cây nhập khẩu',15.0,2500000,'Returned'),
 (5005,'Máy giặt LG Inverter',70.0,9500000,'In Transit');
 
-insert into Delivery_Orders(order_id, shipment_id, shipper_id, assigned_time,shipping_fee,order_status) values
+insert into Delivery_Orders values
 (9001,5001,1,'2024-05-20 08:00:00',2000000,'Processing'),
 (9002,5002,2,'2024-05-20 09:30:00',3500000,'Finished'),
 (9003,5003,3,'2024-05-20 10:15:00',2500000,'Processing'),
 (9004,5004,5,'2024-05-21 07:00:00',1500000,'Finished'),
 (9005,5005,4,'2024-05-21 08:45:00',2500000,'Pending');
 
-insert into Delivery_Log(log_id,order_id,current_location,log_time,note) values
+insert into Delivery_Log values
 (1,9001,'Kho tổng (Hà Nội)','2021-05-15 08:15:00','Rời kho'),
 (2,9001,'Trạm thu phí Phủ Lý','2021-05-17 10:00:00','Đang giao'),
 (3,9002,'Quận 1, TP.HCM','2024-05-19 10:30:00','Đã đến điểm đích'),
